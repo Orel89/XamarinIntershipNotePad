d26ee060209d2af44700da965da8c3bb64e3a50d
oleksii_orel <oleksiiorel@gmail.com>
2021-11-22 17:16:11 +0200

    add pin edit methods

--- Project2/MapNotepad/MapNotepad/ViewModel/PinListViewModel.cs
+++ Project2/MapNotepad/MapNotepad/ViewModel/PinListViewModel.cs
@@ -1,11 +1,16 @@
-﻿using MapNotepad.Helpers;
+﻿using MapNotepad.Extensions;
+using MapNotepad.Helpers;
 using MapNotepad.Model.Pin;
 using MapNotepad.Services.PinService;
 using MapNotepad.Views;
 using Prism.Navigation;
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
 
 namespace MapNotepad.ViewModel
 {
@@ -23,8 +28,8 @@ namespace MapNotepad.ViewModel
         public ICommand AddButtonTapCommand => _addButtonCommand ?? (_addButtonCommand = SingleExecutionCommand.FromFunc(OnAddButtonPinAsync));
 
 
-        private ObservableCollection<PinModel> _observPinCollection;
-        public ObservableCollection<PinModel> ObservPinCollection
+        private ObservableCollection<PinViewModel> _observPinCollection;
+        public ObservableCollection<PinViewModel> ObservPinCollection
         {
             get => _observPinCollection;
             set => SetProperty(ref _observPinCollection, value);
@@ -50,18 +55,35 @@ namespace MapNotepad.ViewModel
 
         private async Task InitAsync()
         {
-            var observUserPinCollection = new ObservableCollection<PinModel>();
+            var observUserPinCollection = new ObservableCollection<PinViewModel>();
             var userPins = await _pinService.GetPinsAsync();
 
-            foreach (var p in userPins.Result)
+            if (userPins.IsSuccess)
             {
-                observUserPinCollection.Add(p);
+                ObservPinCollection = new ObservableCollection<PinViewModel>(userPins.Result.Select(x => x.ToPinViewModel()));
             }
-
-            ObservPinCollection = observUserPinCollection;
-
+            foreach (var pin in ObservPinCollection)
+            {
+                pin.MoveToPinLocationCommand = SingleExecutionCommand.FromFunc<PinViewModel>(GoToPinLocation);
+            }
         }
 
         #endregion
+
+        #region -- Private helpers --
+        private Task GoToPinLocation(PinViewModel pin)
+        {
+            LocatePin(new Position(pin.Latitude, pin.Longitude));
+
+            return Task.CompletedTask;
+        }
+
+        private void LocatePin(Position position)
+        {
+            MessagingCenter.Send(this, "MovePin", position);
+        }
+
+        #endregion
+
     }
 }
