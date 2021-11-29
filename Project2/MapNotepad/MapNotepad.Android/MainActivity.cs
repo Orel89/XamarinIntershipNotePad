db2c3d529c55898ab7c7c99b22167f05a83f2a6c
Oleksii Orel <oleksiiorel@gmail.com>
2021-11-29 19:16:20 +0200

    some fixed

--- Project2/MapNotepad/MapNotepad.Android/MainActivity.cs
+++ Project2/MapNotepad/MapNotepad.Android/MainActivity.cs
@@ -56,6 +56,7 @@ namespace MapNotepad.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Xamarin.FormsGoogleMaps.Init(this, savedInstanceState);
+            global::Xamarin.Auth.Presenters.XamarinAndroid.AuthenticationConfiguration.Init(this, savedInstanceState);
         }
         public class AndroidInitializer : IPlatformInitializer
         {
